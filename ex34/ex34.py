1b56c6e4c815fd5ff78f590aad18b8604f28fdb5
DarinZhang <zdyhajk@126.com>
2019-03-18 22:49:03 +0800

    upload ex34 and ex35

---
+++ ex34/ex34.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1 : python
+print animals[1]
+# The thrid(3rd) animal. peacock
+print animals[2]
+# The first(1st) animal. bear
+print animals[0]
+# The animal at 3. kangaroo
+print animals[3]
+# The fifth(5th) animal. whale
+print animals[4]
+# The animal at 2. peacock
+print animals[2]
+# The sixth(6th) animal. platypus
+print animals[5]
